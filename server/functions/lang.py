4b9ea97f262cfe9cbb06bbfbb85bc969392bf1c3
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-06-28 23:58:06 +0000

    Chore: Gemma Integration

---
+++ server/functions/lang.py
@@ -0,0 +1,7 @@
+import google.generativeai as gena
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_text_splitters import CharcaterTextSplitter
+
+from langchain_community.llms import Ollama
+llm = Ollama(model="gemma2")
+llm.invoke("Why is the sky blue?")
