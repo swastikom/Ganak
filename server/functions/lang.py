2e9b1a3b590e58b2046c4231aa9926ded8e9490f
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-06-29 00:39:47 +0000

    Chore: Changes

--- server/functions/lang.py
+++ server/functions/lang.py
@@ -1,7 +1,10 @@
 import google.generativeai as gena
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_text_splitters import CharcaterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 from langchain_community.llms import Ollama
 llm = Ollama(model="gemma2")
 llm.invoke("Why is the sky blue?")
