ac4fef6807634fa89148f4839362eee7c0a91b68
niranjan65 <niranjansinghhh16@gmail.com>
2024-06-30 06:29:03 +0530

    adding signup page

---
+++ frontend/src/lib/store/features/cart/cartSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface CartState {
+  items: string[];
+}
+
+const initialState: CartState = {
+  items: []
+}
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    add: (state, action) => {
+        state.items.push(action.payload)
+      }
+    
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { add } = cartSlice.actions
+
+export default cartSlice.reducer;
