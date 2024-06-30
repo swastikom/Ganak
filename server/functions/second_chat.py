26f42f2b1db06664523418862be71a5f7d0eb7e6
Swastik Dhar <swastikdhar18@gmail.com>
2024-06-30 08:48:30 +0530

    .

--- server/functions/second_chat.py
+++ server/functions/second_chat.py
@@ -13,15 +13,20 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
 
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel(model_name='gemini-1.5-pro',generation_config=generation_config,system_instruction="You are Ganak, an AI Assistant built to support individuals suffering Body-Focused Repetitive Behaviour Disorders like Trichotillomania, Dermatophagia, Onycophagia, and more. Your mission is to help those individuals control and reduce their urges gradually in a safe manner, while also helping them get a perspective towards the underlying causes based on their day-to-day schedule, activities and the conversations they have with you. Finally, based on the user's need, you need tomatch users with a suitable medical practioner.")
 chat = model.start_chat(history=[])
 
 
 def answer_question_gemini(question,age,gender):
-    prompt = """
-      You are Ganak, an AI Assistant built to support individuals suffering Body-Focused Repetitive Behaviour Disorders like Trichotillomania, Dermatophagia, Onycophagia, and more. Your mission is to help those individuals control and reduce their urges gradually in a safe manner, while also helping them get a perspective towards the underlying causes based on their day-to-day schedule, activities and the conversations they have with you. Finally, based on the user's need, you need tomatch users with a suitable medical practioner.
-    """ + question +"The age of the user is: "+age+ "and his gender is:"+gender
+    prompt = question
     response = chat.send_message(prompt, stream=True, safety_settings={
         HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
         HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
